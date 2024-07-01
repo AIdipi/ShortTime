ae6f17a9b3277b60ffb24526e56fa5ed7f232dd1
yooni1231 <138295473+yooni1231@users.noreply.github.com>
2024-07-01 14:57:28 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,68 @@
+from flask import Flask, render_template, request, redirect, url_for, send_from_directory
+import os
+from werkzeug.utils import secure_filename
+import subprocess
+import cv2
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'uploads'
+app.config['ALLOWED_EXTENSIONS'] = {'mp4'}
+app.config['RESULTS_FOLDER'] = 'results'
+
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.makedirs(app.config['UPLOAD_FOLDER'])
+
+if not os.path.exists(app.config['RESULTS_FOLDER']):
+    os.makedirs(app.config['RESULTS_FOLDER'])
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in app.config['ALLOWED_EXTENSIONS']
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/upload', methods=['POST'])
+def upload_file():
+    if 'file' not in request.files:
+        return redirect(request.url)
+    file = request.files['file']
+    if file.filename == '':
+        return redirect(request.url)
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file_path = os.path.join(app.config['UPLOAD_FOLDER'], filename)
+        file.save(file_path)
+
+        # Run the tracking script
+        reid_model_path = 'osnet_x0_25_msmt17.pt'
+        project_path = app.config['RESULTS_FOLDER']
+        name = filename.rsplit('.', 1)[0]  # Use the file name without extension as the result directory name
+        result_dir = os.path.join(project_path, name)
+        if not os.path.exists(result_dir):
+            os.makedirs(result_dir)
+
+        try:
+            command = f'python tracking/track.py --source {file_path} --reid-model {reid_model_path} --save --project {project_path} --name {name} --save-id-crops --show-conf'
+            subprocess.check_call(command, shell=True)
+        except subprocess.CalledProcessError as e:
+            return f"An error occurred: {e}"
+    
+        return redirect(url_for('result',name= name))
+
+
+@app.route('/result')
+def result():
+    name = request.args.get('name')
+    video_path = os.path.join(name, 'output.avi')  # Construct the path to the output video file relative to the RESULTS_FOLDER
+    return render_template('result.html', video_path=video_path)
+
+@app.route('/results/<path:filename>')
+def download_file(filename):
+    return send_from_directory(app.config['RESULTS_FOLDER'], filename)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
